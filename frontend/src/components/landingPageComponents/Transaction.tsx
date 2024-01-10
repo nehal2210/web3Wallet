33be08602f1cf56c4d9e36ff017f72c46ab9581b
umair <124577697+umairqureshikhan@users.noreply.github.com>
2024-01-10 20:23:44 +0500

    remove colon

--- frontend/src/components/landingPageComponents/Transaction.tsx
+++ frontend/src/components/landingPageComponents/Transaction.tsx
@@ -9,22 +9,22 @@ const Transaction = () => {
         <div className="w-[25%] ">
           <img src={transactionimage} alt="transactionimages" />
           <p className="mt-8">
-            Seamlessly transact between native currency and ERC20 tokens:
-            sending and receiving made effortless.
+            Seamlessly transact between native currency and ERC20 tokens
+            sending and receiving made effortless
           </p>
         </div>
         <div className="w-[25%]">
           <img src={contactimage} alt="transactionimages" />
           <p className="mt-8">
-            Create a multitude of secure accounts within one wallet: your
+            Create a multitude of secure accounts within one wallet your
             personalized vault for diversified assets
           </p>
         </div>
         <div className="w-[25%]">
           <img src={netwokimage} alt="transactionimages" />
           <p className="mt-8">
-            Expand your horizons: add a plethora of Ethereum-compatible networks
-            with ease.
+            Expand your horizons add a plethora of Ethereum-compatible networks
+            with ease
           </p>
         </div>
       </div>
